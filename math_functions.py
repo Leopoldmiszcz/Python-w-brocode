c5687d6224359f5e2a186ebee68fa821f2390a47
Leopold <leopoldmiszcz@outlook.com>
2023-07-07 23:06:18 +0200

    Day 1 of learning python!

---
+++ math_functions.py
@@ -0,0 +1,16 @@
+import math
+
+pi = 3.14
+
+x = 1
+y = 2
+z = 3
+
+print(round(pi))
+print(math.ceil(pi)) #rounds variable up
+print(math.floor(pi)) #rounds variable down
+print(abs(pi)) #gives absolute value of variable (how far that number is from zero)
+print(pow(pi, 2)) #powers to the two
+print(math.sqrt(42)) #square roots a number
+print(max(x, y, z)) #findes the largest value
+print(min(x, y, z)) #findes the lowest value
---
+++ string_slicing.py
@@ -0,0 +1,23 @@
+# slicing = create a substring by extracting elements from another string
+
+name = "Leopold Kaniewski"
+
+#[start:stop:step]
+first_name = name[0:7]
+last_name = name[8:]
+funky_name = name[0:7:2]
+reversed_name = name[::-1]
+
+print(first_name)
+print(last_name)
+print(funky_name)
+print(reversed_name)
+
+
+website1 = "http://google.com"
+website2 = "http://wikipedia.com"
+
+slice = slice(7, -4)
+
+print(website1[slice])
+print(website2[slice])
