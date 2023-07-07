782701ba1589c0518b027aefd4d3ab4c7b57e04b
Leopold Kaniewski <leopoldmiszcz@outlook.com>
2023-07-07 12:33:37 +0200

    Second commit

---
+++ type_casting.py
@@ -0,0 +1,12 @@
+#type casting = convert the datatype of a value to another data type
+
+x = 1 #int
+y = 2.0 #float
+z = "3" #str
+
+
+y = int(y) #Changes var "y" to an int
+
+print(x)
+print(y)
+print(z)
